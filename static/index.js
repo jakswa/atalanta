cf5bb38905a84fdd577cebc59b1ae4ea8b847af3
Jake Swanson <jakswa@gmail.com>
2024-04-12 22:47:00 -0400

    dummy text, dummy route drawn, responsive, dark mode

--- static/index.js
+++ static/index.js
@@ -1,9 +1,64 @@
-onReady(initMap);
+onReady(init);
 
-const TILES = 'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm-lite/{z}/{x}/{y}.png';
+async function init() {
+  let map = initMap();
+
+  let trkpts = (await routeXML()).querySelectorAll('trkpt');
+  let routeCoords = routeCoordinates(trkpts);
+  map.addSource('route-line', { type: 'geojson', data: routeData(routeCoords) });
+  map.addLayer(routeLayer());
+  map.fitBounds([routeCoords[0], routeCoords[routeCoords.length - 1]], { padding: 60 });
+}
+
+function routeLayer() {
+  return {
+    id: 'route-layer',
+    type: 'line',
+    source: 'route-line',
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    paint: {
+      'line-color': '#00FF00',
+      'line-width': 6,
+      
+    }
+  };
+}
+
+function routeCoordinates(trkpts) {
+  let coordinates = [];
+  for (let i = 0; i < trkpts.length; i++) {
+    let trkpt = trkpts[i];
+    coordinates.push([
+      parseFloat(trkpt.getAttribute('lon')),
+      parseFloat(trkpt.getAttribute('lat'))
+    ]);
+  }
+  return coordinates;
+}
+
+function routeData(coordinates) {
+  return {
+    type: 'FeatureCollection',
+    features: [{
+      geometry: {
+        type: 'LineString',
+        coordinates
+      }
+    }]
+  };
+}
+
+async function routeXML() {
+  return await fetch('/route.gpx')
+    .then((res) => res.text())
+    .then((xml) => new DOMParser().parseFromString(xml, 'application/xml'));
+}
 
 function initMap() {
-  var map = new maplibregl.Map({
+  return new maplibregl.Map({
     container: 'map',
     //style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
     style: {
@@ -24,6 +79,7 @@ function initMap() {
           type: 'raster',
           source: 'osm',
           minzoom: 0,
+          paint: { "raster-opacity": 0.5 },
           maxzoom: 22
         }
       ]
@@ -40,3 +96,4 @@ function onReady(fn) {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
+
